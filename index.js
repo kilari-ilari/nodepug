690f7481fd2ee204290e22409a30f0aaa0bddd3f
IlariT <bgh310@myy.haaga-helia.fi>
2023-06-02 14:24:38 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express'); 
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const port = 3000;
+
+// asettaa käytettävän mallimoottorin ja Node.js lataa pug-moduulin
+app.set('view engine', 'pug')
+
+let movies = [
+  {id: '1588323375416', title: 'Star Wars: Episode IX - The Rise of Skywalker', year: 2019, director: 'J.J. Abrams'},
+  {id: '1588323390624', title: 'The Irishman', year: 2019, director: 'Martin Scorsese'},
+  {id: '1588323412643', title: 'Harry Potter and the Sorcerers Stone', year: 2001, director: 'Chris Columbus'}
+];
+
+app.get("/hello", (req, res) => {
+  res.render("hello", {firstname: 'Matti', lastname: 'Mallikas'},);
+})
+
+// juuri
+app.get("/", (req, res) => {
+  res.render("movielist", {movies: movies});
+})
+
+app.get("/addmovie", (req, res) => {
+  res.render("addmovie");
+})
+
+app.post("/addmovie", (req, res) => {
+  const newMovie = {id: new Date().now, title: req.body.title, director: req.body.director, year: req.body.year};
+  movies = [...movies, newMovie];
+  res.redirect("/movies");
+})
+
+// Include helmet
+const helmet = require('helmet');
+app.use(helmet());
+
+app.listen(process.env.PORT || 3000, () => {
+  console.log(`Server is running on port ${port}.`);
+});
